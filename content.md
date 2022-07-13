498c7e07956b7a3a3088a22d88136250984eec18
iamhammyboi19 <ahmedadekoya2000@gmail.com>
2022-07-13 11:36:32 +0300

    initial commit

---
+++ content.md
@@ -0,0 +1,169 @@
+<!-- Hardworking College Student seeking employment.• Bringing forth a motivated attitude and a variety of powerful skills.• Adept in various social media platforms and office technology programs.• Committed to utilizing my skills to further the mission of a company.
+Dedicated Customer Service Representative dedicated to providing quality care for ultimate customer satisfaction.• Proven ability to establish and maintain excellent communication and relationships with clients.• Adept in general accounting and finance transactions.• Dedicated to identifying customer needs and delivering effective solutions to all problems.• Excellent time management skills combined with a superior knowledge of the customer service industry.• Bilingual, hardworking, and ready to join my next team.
+Dedicated Nursing Student with the ability to multitask and work well with others.• Patient-care oriented, bringing forth a compassionate and friendly attitude.• Highly organized, and skilled in written and verbal communication.• Committed to utilizing my skills to help others, while working towards the mission of a company.• A strong leader who works well under pressure, and exudes positivity.
+Resourceful and dedicated High School student with excellent analytical skills and a demonstrated commitment to providing great customer service.• Strong organizational abilities with proven successes managing multiple academic projects and volunteering events.• Well-rounded and professional team player dedicated to continuing academic pursuits at a collegiate level.
+Experienced Software Developer adept in bringing forth expertise in design, installation, testing and maintenance of software systems.• Proficient in various platforms, languages, and embedded systems.• Experienced with the latest cutting edge development tools and procedures.• Able to effectively self-manage during independent projects, as well as collaborate as part of a productive team.
+Dynamic Executive with six years of experience helping organizations reach their full potential.• Adept in making key decisions and working with other professionals to achieve goals and solve problems.• Experienced in managing employee and community programs, and dedicated to successfully directing business operations.
+Experienced Web Developer adept in all stages of advanced web development.• Knowledgeable in user interface, testing, and debugging processes.• Bringing forth expertise in design, installation, testing and maintenance of web systems.• Equipped with a diverse and promising skill-set.• Proficient in an assortment of technologies, including Java, ASP.NET, C#, IIS, Tomcat, and Microsoft SQL Server.• Able to effectively self-manage during independent projects, as well as collaborate in a team setting.
+Experienced and self-motivated Office Secretary with ten+ years of industry experience overseeing the main offices of schools.• Highly competent communicator skilled in multitasking and effectively communicating with others.• Bringing forth a proven track record of successfully managing offices, and helping to lead school professionals to work toward reaching goals.
+Experienced and dedicated Data Analyst with several years of experience identifying efficiencies and problem areas within data streams, while communicating needs for projects.• Adept at receiving and monitoring data from multiple data streams, including Access, SQL, and Excel data sources.• Bringing forth the ability to synthesize quantitative information and interact effectively with colleagues and clients.• Proven track record of generating summary documents for senior management for monthly and quarterly audit and compliance reporting.
+Dedicated Visual Artist with a decorated background in a variety of artistic mediums.• Highly experienced in illustration and animation.• Adept at working independently and collaboratively on projects, and committed to achieving visual innovation and beauty.• Bringing forth the ability to work well with others and utilize my art skills to visually enhance projects. -->
+
+<!-- <div class="wrapper">
+    <div class="fixed-wrapper">
+        <div class="fixed">
+            Content in here will be fixed position, but 240px to the left of the site body.
+        </div>
+    </div>
+</div> -->
+
+<!-- /* Main site body */
+.wrapper {
+    width: 940px;
+    margin: 0 auto;
+    position: relative; /* Ensure absolute positioned child elements are relative to this*/
+}
+
+/* Absolute positioned wrapper for the element you want to fix position */
+.fixed-wrapper {
+    width: 220px;
+    position: absolute;
+    top: 0;
+    left: -240px; /* Move this out to the left of the site body, leaving a 20px gutter */
+}
+
+/* The element you want to fix the position of */
+.fixed {
+    width: 220px;
+    position: fixed;
+    /* Do not set top / left! */
+} -->
+
+<!-- // const hideSuggesstBoxFunc = function () {
+//   suggestionBoxDiv.classList.toggle("hide-suggestion-box");
+//   showSuggestBox.classList.toggle("hide-open-close-btn");
+//   hideSuggestBox.classList.toggle("hide-open-close-btn");
+// }; -->
+
+<!-- .text-styles-con-div {
+  width: 100%;
+}
+.text-styles-con-div {
+  position: relative;
+  overflow: visible;
+}
+.suggestion-texts-con-div {
+  max-width: 50rem;
+  position: absolute;
+  left: 98%;
+  top: 10%;
+  display: block;
+  margin-left: 3rem;
+  transform: translateX(-2%);
+  transition: all 0.4s ease-in-out;
+  opacity: 0;
+  pointer-events: none;
+  visibility: hidden;
+}
+
+.suggestion-con {
+  max-width: 50rem;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  flex-shrink: 0;
+  justify-content: center;
+  background-color: #fff;
+  padding: 2rem 2rem;
+  box-shadow: 0 3.2rem 5.4rem rgba(0, 0, 0, 0.06);
+  border: 1px solid #868e96;
+  border-radius: 5px;
+}
+
+.hide-suggestion-box {
+  opacity: 1;
+  pointer-events: auto;
+  visibility: visible;
+  transform: translateX(0);
+}
+.suggestion-texts {
+  font-size: 1.4rem;
+  line-height: 1.8;
+  padding: 0.05rem 0.05rem;
+  font-weight: 300;
+}
+.suggestion-texts-con {
+  width: 50rem;
+}
+.arrow-btn {
+  font-size: 2rem;
+}
+.close-arrow-btn {
+  font-size: 2rem;
+  color: #2f9e44;
+}
+.close-arrow-btn-con {
+  display: block;
+  height: 4rem;
+  width: 4rem;
+  background-color: #b2f2bb;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  align-self: flex-start;
+}
+.arrow-btn-con {
+  display: block;
+  height: 4rem;
+  width: 4rem;
+  background-color: #dee2e6;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  align-self: flex-start;
+}
+.suggestion-texts:hover {
+  background-color: #d0ebff;
+  color: #1971c2;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+.arrow-btn-con:hover {
+  color: #1971c2;
+  background-color: #d0ebff;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+.arrow-btn-con:hover + div .suggestion-texts {
+  color: #1971c2;
+  cursor: pointer;
+  transition: all 0.3s ease;
+} -->
+
+  <!-- <div class="suggestion-texts-con-div-edu">
+                <aside class="suggestion-con-edu">
+                  <span class="close-arrow-btn-con-edu hide-open-close-btn"
+                    ><ion-icon
+                      name="checkmark-outline"
+                      class="close-arrow-btn-edu"
+                    ></ion-icon
+                  ></span>
+                  <span class="arrow-btn-con-edu"
+                    ><ion-icon
+                      name="arrow-back-outline"
+                      class="arrow-btn-education"
+                    ></ion-icon>
+                  </span>
+
+                  <div class="suggestion-texts-con-edu">
+                    <span class="suggestion-texts-education"
+                      >Worked to ensure a positive and hassle-free customer
+                      experience.</span
+                    >
+                  </div>
+                </aside>
+   </div> -->
---
+++ inputScript.js
@@ -0,0 +1,372 @@
+"use strict";
+
+const hiddenForms = document.querySelectorAll(".hidden-input-divs");
+const showFormCon = document.querySelector(".show-form-container");
+const hidFormCon = document.querySelector(".hide-form-container");
+
+// -----------------------------------------------------------------------------------
+
+//  suggestion box and text parts
+const showSuggestBox = document.querySelector(".pre-written-profession");
+const hideSuggestBox = document.querySelector(".close-pre-con");
+const showSuggestBoxEmploy = document.querySelector(".pre-written-employment");
+const hideSuggestBoxEm = document.querySelector(".close-pre-con-employment");
+const suggestionBoxDiv = document.querySelector(".suggestion-texts-con-div");
+const theBoxTexts = document.querySelector(".suggestion-texts-con");
+const theBoxTextsEmploy = document.querySelector(".suggestion-texts-con-emp");
+const suggestionBoxDivEmploy = document.querySelector(
+  ".suggestion-texts-con-div-emp"
+);
+const textareaInput = document.querySelector(".summary-input");
+const textareaEmploy = document.querySelector(".summary-input-emp");
+const prePhraseArrowBtn = document.querySelector(".arrow-btn-con");
+const prePhraseArrowBtnEmp = document.querySelector(".arrow-btn-con-emp");
+const suggestionTexts = document.querySelectorAll(".suggestion-texts");
+const suggestionTextsEmp = document.querySelectorAll(
+  ".suggestion-texts-employment"
+);
+const prePhraseCloseArrowBtn = document.querySelector(".close-arrow-btn-con");
+const prePhraseCloseArrowBtnEmp = document.querySelector(
+  ".close-arrow-btn-con-emp"
+);
+
+// ------------------------------------------------------------------------------------
+
+//  form calendar part first calendar
+const calendarDiv = document.querySelector(".calendar-div");
+const calendarYear = document.querySelector(".calendar-year");
+const calendarBackFront = document.querySelector(".calendar-back-front");
+const deductCurYear = document.querySelector(".calendar-back-pointer");
+const addCurYear = document.querySelector(".calendar-front-pointer");
+const calendarMonths = document.querySelector(".calendar-months-con");
+const startDate = document.querySelector(".startdate");
+
+// second calendar
+const calendarDivEdu = document.querySelector(".calendar-div-edu");
+const calendarYearEdu = document.querySelector(".calendar-year-edu");
+const calendarBackFrontEdu = document.querySelector(".calendar-back-front-edu");
+const deductCurYearEdu = document.querySelector(".calendar-back-pointer-edu");
+const addCurYearEdu = document.querySelector(".calendar-front-pointer-edu");
+const calendarMonthsEdu = document.querySelector(".calendar-months-con-edu");
+const startDateEdu = document.querySelector(".startdate-edu");
+
+// ------------------------------------------------------------------------------------
+
+// employment form parts
+const employmentSection = document.querySelector(".employment-details--");
+const jobTitleUpdate1 = document.querySelector(".job-title-update-con-1");
+const jobTitleUpdate2 = document.querySelector(".job-title-update-con-2");
+const employmentForm = document.querySelector(".employment-form");
+const employmentTextStyle = document.querySelector(
+  ".text-styles-con-employment"
+);
+const addEmploymentBtn = document.querySelector(".employment-div");
+const employmentJobTitleInp = document.querySelector(
+  ".employment-job-title-inp"
+);
+const jobtitleUpdatetext1 = document.querySelector(".job-title-update-1");
+const jobtitleUpdatetext2 = document.querySelector(".job-title-update-2");
+const inputDivJob = document.querySelector(".input-div-job");
+
+// ------------------------------------------------------------------------------------
+
+// // education form parts
+const educationSection = document.querySelector(".edu-details--");
+const schTitleUpdate1 = document.querySelector(".job-title-update-con-1-edu");
+const schTitleUpdate2 = document.querySelector(".job-title-update-con-2-edu");
+const educationForm = document.querySelector(".education-form");
+const educationTextStyle = document.querySelector(".text-styles-con-education");
+const addEducationBtn = document.querySelector(".education-div");
+const educationJobTitleInp = document.querySelector(".education-job-title-inp");
+const schtitleUpdatetext1 = document.querySelector(".job-title-update-1-edu");
+const schtitleUpdatetext2 = document.querySelector(".job-title-update-2-edu");
+const inputDivSchool = document.querySelector(".input-div-school");
+
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+
+const thedate = new Date();
+let curDate = thedate.getFullYear();
+calendarYear.textContent = curDate;
+
+const theDate2 = new Date();
+let curDateEdu = theDate2.getFullYear();
+calendarYearEdu.textContent = curDateEdu;
+
+// ------------------------------------------------------------------------------------
+
+const hideShowFormCon = function () {
+  [...hiddenForms].forEach((e) => e.classList.toggle("hidden-form-container"));
+  hidFormCon.classList.toggle("hidden-form-container");
+  showFormCon.classList.toggle("hidden-form-container");
+};
+
+// hide and show the first form container
+showFormCon.addEventListener("click", hideShowFormCon);
+hidFormCon.addEventListener("click", hideShowFormCon);
+
+// ------------------------------------------------------------------------------------
+
+const showHideSuggestionBoxFunc = function () {
+  suggestionBoxDiv.classList.toggle("hide-suggestion-box");
+  showSuggestBox.classList.toggle("hide-open-close-btn");
+  hideSuggestBox.classList.toggle("hide-open-close-btn");
+};
+
+// hide and show the first suggestion text container
+showSuggestBox.addEventListener("click", showHideSuggestionBoxFunc);
+hideSuggestBox.addEventListener("click", showHideSuggestionBoxFunc);
+
+// ------------------------------------------------------------------------------------
+
+const showHideSuggestBoxEmployFunc = function () {
+  suggestionBoxDivEmploy.classList.toggle("hide-suggestion-box-emp");
+  showSuggestBoxEmploy.classList.toggle("hide-open-close-btn");
+  hideSuggestBoxEm.classList.toggle("hide-open-close-btn");
+};
+
+// hide and show the second suggestion text container
+showSuggestBoxEmploy.addEventListener("click", showHideSuggestBoxEmployFunc);
+hideSuggestBoxEm.addEventListener("click", showHideSuggestBoxEmployFunc);
+
+// ------------------------------------------------------------------------------------
+
+const textareaFill = function (e) {
+  if (!e.target.classList.contains(`${this[0]}`)) return;
+  const thetextContent = e.target.textContent.split(" ");
+  const theevent = e.target;
+  theevent.style.pointerEvents = "none";
+  theevent.style.color = "#868e96";
+  const thefiltered = thetextContent.filter((r) => r.length > 0);
+  const filtered2 = thefiltered.filter((r) => r.trim());
+  const filtered3 = filtered2.filter((r) => {
+    if (!r.endsWith("\n")) return r;
+  });
+  const thejoinedTextContent = filtered3.join(" ");
+  this[1].textContent += thejoinedTextContent + " ";
+};
+
+theBoxTexts.addEventListener(
+  "click",
+  textareaFill.bind(["suggestion-texts", textareaInput])
+);
+
+theBoxTextsEmploy.addEventListener(
+  "click",
+  textareaFill.bind(["suggestion-texts-employment", textareaEmploy])
+);
+
+// ------------------------------------------------------------------------------------
+
+const textareaFillWhole = function (e) {
+  const thetextContent = this[0].textContent.split(" ");
+  [...this[1]].forEach((e) => {
+    e.style.pointerEvents = "none";
+    e.style.color = "#868e96";
+  });
+  this[2].classList.toggle("hide-open-close-btn");
+  this[3].classList.toggle("hide-open-close-btn");
+  const thefiltered = thetextContent.filter((r) => r.length > 0);
+  const filtered2 = thefiltered.filter((r) => r.trim());
+  const filtered3 = filtered2.filter((r) => {
+    if (!r.endsWith("\n")) return r;
+  });
+  const thejoinedTextContent = filtered3.join(" ");
+  this[4].textContent += thejoinedTextContent + "." + " ";
+};
+
+// theBoxTexts suggestionTexts prePhraseArrowBtn prePhraseCloseArrowBtn textareaInput
+// fill textarea at once for first suggestion text
+prePhraseArrowBtn.addEventListener(
+  "click",
+  textareaFillWhole.bind([
+    theBoxTexts,
+    suggestionTexts,
+    prePhraseArrowBtn,
+    prePhraseCloseArrowBtn,
+    textareaInput,
+  ])
+);
+
+// theBoxTextsEmploy suggestionTextsEmp prePhraseArrowBtnEmp prePhraseCloseArrowBtnEmp textareaEmploy
+// fill textarea at once for second suggestion text
+prePhraseArrowBtnEmp.addEventListener(
+  "click",
+  textareaFillWhole.bind([
+    theBoxTextsEmploy,
+    suggestionTextsEmp,
+    prePhraseArrowBtnEmp,
+    prePhraseCloseArrowBtnEmp,
+    textareaEmploy,
+  ])
+);
+// ------------------------------------------------------------------------------------
+
+// employment calendar
+employmentForm.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("startdate")) return;
+  calendarDiv.classList.toggle("hide-show-calendar-div");
+});
+
+let curYear = curDate;
+
+deductCurYear.addEventListener("click", function () {
+  +curYear--;
+  calendarYear.textContent = curYear;
+});
+
+addCurYear.addEventListener("click", function () {
+  +curYear++;
+  calendarYear.textContent = curYear;
+});
+
+calendarYear.addEventListener("click", function () {
+  startDate.value = calendarYear.textContent;
+});
+
+calendarMonths.addEventListener("click", function (e) {
+  if (e.target.classList.contains("calendar-months-con")) return;
+  startDate.value = e.target.textContent + "," + " " + calendarYear.textContent;
+  calendarDiv.classList.toggle("hide-show-calendar-div");
+});
+
+// ------------------------------------------------------------------------------------
+
+// education calendar
+educationForm.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("startdate-edu")) return;
+  calendarDivEdu.classList.toggle("hide-show-calendar-div");
+});
+
+let curYearEdu = curDateEdu;
+
+deductCurYearEdu.addEventListener("click", function () {
+  +curYearEdu--;
+  calendarYearEdu.textContent = curYearEdu;
+});
+
+addCurYearEdu.addEventListener("click", function () {
+  +curYearEdu++;
+  calendarYearEdu.textContent = curYearEdu;
+});
+
+calendarYearEdu.addEventListener("click", function () {
+  startDateEdu.value = calendarYearEdu.textContent;
+});
+
+calendarMonthsEdu.addEventListener("click", function (e) {
+  if (e.target.classList.contains("calendar-months-con-edu")) return;
+  startDateEdu.value =
+    e.target.textContent + "," + " " + calendarYearEdu.textContent;
+  calendarDivEdu.classList.toggle("hide-show-calendar-div");
+});
+
+// ------------------------------------------------------------------------------------
+
+// show employment section
+addEmploymentBtn.addEventListener("click", function () {
+  employmentSection.classList.remove("hidden");
+});
+
+const showHideEmploymentform = function () {
+  employmentForm.classList.toggle("hidden");
+  jobTitleUpdate1.classList.toggle("hidden");
+  jobTitleUpdate2.classList.toggle("hidden");
+  employmentTextStyle.classList.toggle("hidden");
+};
+
+jobTitleUpdate1.addEventListener("click", showHideEmploymentform);
+
+jobTitleUpdate2.addEventListener("click", showHideEmploymentform);
+
+employmentJobTitleInp.addEventListener("input", function (e) {
+  const jobTitleObs = e.target.value;
+  if (jobTitleObs.length === 0) {
+    jobtitleUpdatetext1.textContent = jobtitleUpdatetext2.textContent =
+      "(Not Specfied)";
+  } else {
+    jobtitleUpdatetext1.textContent = jobtitleUpdatetext2.textContent =
+      jobTitleObs;
+  }
+});
+// ------------------------------------------------------------------------------------
+
+// show education section
+addEducationBtn.addEventListener("click", function () {
+  educationSection.classList.remove("hidden");
+});
+
+const showHideEducationform = function () {
+  educationForm.classList.toggle("hidden");
+  schTitleUpdate1.classList.toggle("hidden");
+  schTitleUpdate2.classList.toggle("hidden");
+  educationTextStyle.classList.toggle("hidden");
+};
+
+schTitleUpdate1.addEventListener("click", showHideEducationform);
+
+schTitleUpdate2.addEventListener("click", showHideEducationform);
+
+educationJobTitleInp.addEventListener("input", function (e) {
+  const sclTitleObs = e.target.value;
+  if (sclTitleObs.length === 0) {
+    schtitleUpdatetext1.textContent = schtitleUpdatetext2.textContent =
+      "(Not Specfied)";
+  } else {
+    schtitleUpdatetext1.textContent = schtitleUpdatetext2.textContent =
+      sclTitleObs;
+  }
+});
+
+// ------------------------------------------------------------------------------------
+
+// hiding positioned element clicking outing the element PROFESSIONAL
+
+// LOGIC
+/*
+1) make sure the element you are hiding and showing and the buttons doing it are not responding while clicking on body element
+2) hide the box as long as the first condition is not met
+3) make a new condition to check if the box is hidden 
+4) hide the close->(hide-suggestion-box-btn) btn under the second condition
+5) show the open->(show-suggestion-box-btn) btn under the second condition
+
+*/
+
+const showHideSuggestionBoxProf = function (e) {
+  if (
+    e.target.closest(".suggestion-texts-con-div") ||
+    e.target.closest(".pre-written-profession") ||
+    e.target.closest(".close-pre-con")
+  )
+    return;
+  suggestionBoxDiv.classList.remove("hide-suggestion-box");
+  if (!suggestionBoxDiv.classList.contains("hide-suggestion-box")) {
+    showSuggestBox.classList.remove("hide-open-close-btn");
+    hideSuggestBox.classList.add("hide-open-close-btn");
+  }
+};
+
+document.documentElement.addEventListener("click", showHideSuggestionBoxProf);
+
+// hiding positioned element clicking outing the element EMPLOY
+
+// suggestion-texts-con-div-emp pre-written-employment close-pre-con-employment hide-suggestion-box-emp
+
+const showHideSuggestionBoxEmploy = function (e) {
+  if (
+    e.target.closest(".suggestion-texts-con-div-emp") ||
+    e.target.closest(".pre-written-employment") ||
+    e.target.closest(".close-pre-con-employment")
+  )
+    return;
+  suggestionBoxDivEmploy.classList.remove("hide-suggestion-box-emp");
+  if (!suggestionBoxDivEmploy.classList.contains("hide-suggestion-box-emp")) {
+    showSuggestBoxEmploy.classList.remove("hide-open-close-btn");
+    hideSuggestBoxEm.classList.add("hide-open-close-btn");
+  }
+};
+document.documentElement.addEventListener("click", showHideSuggestionBoxEmploy);
+
+// hiding positioned element clicking outing the element CALENDAR
+
+// ------------------------------------------------------------------------------------
